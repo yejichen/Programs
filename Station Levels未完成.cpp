f8979b8fcf3ea41733126431e1f14e3285f8ff8a
yejichen <yejichen2@sina.cn>
2017-08-08 12:57:52 +0800

    Create Station Levels未完成.cpp

---
+++ Station Levels未完成.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int n,m,i,j;
+    int s[m];
+    for(i=0;i<m;i++)
+    {
+        cin>>s[i];
+        for(j=0;j<s[i];j++)
+        {
+            cin>>        
+        }
+    }
+}
