710a8766394ee9f0a0e1b2550cda6c668ec5a03b
yejichen <yejichen2@sina.cn>
2017-11-05 16:25:12 +0800

    Create CHOICE.cpp

---
+++ CHOICE.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+struct kz
+{
+	int col;
+	int pri;
+};
+int main()
+{
+	int n,k,p;
+	cin>>n>>k>>p;
+	kz k[200005];
+	for(i=1;i<=n;i++)
+	{
+		cin>>k.col>>k.pri;
+		
+	}
+	
+}
